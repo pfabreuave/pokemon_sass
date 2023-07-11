fe53ef9e3564c4eb1dff00331ab28b38a4ec44c9
Pedro Abreu <pfabreuave.gmail.com>
2023-07-11 15:59:36 -0300

    se agrega el resto de lo archivos

---
+++ js/app.js
@@ -0,0 +1,72 @@
+var pokem
+function proceso(){
+    console.log("entro")
+}
+document.addEventListener('DOMContentLoaded', function(event) {
+     var btn = addEventListener("click", function(e) {
+            e.preventDefault();
+            var btn = document.querySelector("#valor");
+            pokem = btn.value
+            fetchData(pokem)
+            document.getElementById("formulario").reset();
+     })
+        const pokemon = {
+            id:'',
+            img: 
+            'https://i.pinimg.com/originals/7d/5c/18/7d5c18be5689eebd1896363ff7117fe0.jpg',
+            nombre: 'Escolha seu pokémon',
+            hp: '',
+            experiencia: '',
+            ataque: '00',
+            especial: '00',
+            defensa:'00',
+            numero: '',
+            peso: '',
+            fuerza: '',
+            experto: 'Experimente'
+        }
+        dibujaCard(pokemon)
+})
+const fetchData = async (id) => {
+    try 
+        {   
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            const data = await res.json()
+            const pokemon = {
+                id: data.id,
+                img: data.sprites.other.dream_world.front_default,
+                nombre: data.name,
+                hp: data.stats[0].base_stat,
+                experiencia: data.base_experience,
+                ataque: data.stats[1].base_stat,
+                especial: data.stats[3].base_stat,
+                defensa: data.stats[2].base_stat,
+                numero: '#',
+                peso: 'K',
+                fuerza: 'Hp',
+                experto: 'Exp'
+            }
+            console.log(pokemon)
+            dibujaCard(pokemon)
+        
+        } catch (error) {
+            //alert(" Não tenho, tente outro")
+        }
+}
+
+const dibujaCard = (pokemon) => {
+    const flex = document.querySelector('.flex')
+    const template = document.querySelector('#template-card').content
+    const clone = template.cloneNode(true)
+    const fragment = document.createDocumentFragment()
+    clone.querySelector('.card-body-img').setAttribute('src', pokemon.img)
+    clone.querySelector('.card-body-title').innerHTML = `${pokemon.numero} ${pokemon.id} ${pokemon.nombre} <span>${pokemon.hp} ${pokemon.fuerza}</span>`
+    clone.querySelector('.card-body-text').textContent = pokemon.experiencia + pokemon.experto
+    clone.querySelectorAll('.card-footer-social h3')[0].textContent = pokemon.ataque + pokemon.peso
+    clone.querySelectorAll('.card-footer-social h3')[1].textContent = pokemon.especial + pokemon.peso
+    clone.querySelectorAll('.card-footer-social h3')[2].textContent = pokemon.defensa + pokemon.peso
+    fragment.appendChild(clone)
+    flex.appendChild(fragment)
+    document.getElementById("formulario").reset();
+ }
+ 
